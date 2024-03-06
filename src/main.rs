c85e859b15266ada53b6a3e345dcb905cc9b4a5c
Eivind Alexander Bergem <eivind.bergem@inventas.no>
2024-03-06 14:31:41 +0100

    Basic MVP

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::{io::{self, Read, Write}, sync::Mutex};
+
+use clap::Parser;
+use termion::raw::IntoRawMode;
+
+#[derive(Parser, Debug)]
+struct Args {
+    baud_rate: u32,
+    #[arg()]
+    device: String,
+}
+
+
+fn main() {
+    let args = Args::parse();
+    let sp = Mutex::new(serialport::new(args.device, args.baud_rate).open().unwrap());
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+
+    std::thread::scope(|s| {
+        s.spawn(|| {
+            loop {
+                let mut buf = [0; 128];
+                let count = stdin.read(&mut buf).unwrap();
+                let data = &buf[..count];
+
+                let _ = sp.lock().unwrap().write(data).unwrap();
+            }
+        });
+
+        loop {
+            let mut buf = [0; 128];
+            let count = sp.lock().unwrap().read(&mut buf).unwrap();
+            let data = &buf[..count];
+
+            let _ = stdout.write(data).unwrap();
+        }
+    });
+}
