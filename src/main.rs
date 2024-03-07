622153bb3cfc24ac7fb9ed97461bb7f1a764055c
Eivind Alexander Bergem <eivind.bergem@inventas.no>
2024-03-07 09:59:57 +0100

    Set read and write timeouts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{io::{self, Read, Write}, sync::Mutex};
+use std::{
+    io::{self, Read, Write},
+    sync::Mutex,
+    time::Duration,
+};
 
 use clap::Parser;
 use crossterm::terminal;
@@ -11,37 +15,43 @@ struct Args {
     device: String,
 }
 
-
 fn main() {
     let args = Args::parse();
-    let sp = Mutex::new(SerialPort::open(args.device, args.baud_rate).unwrap());
+    let mut sp = Mutex::new(SerialPort::open(args.device, args.baud_rate).unwrap());
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
+
+    sp.get_mut()
+        .unwrap()
+        .set_read_timeout(Duration::from_secs(1))
+        .unwrap();
+    sp.get_mut()
+        .unwrap()
+        .set_write_timeout(Duration::from_secs(1))
+        .unwrap();
 
     terminal::enable_raw_mode().unwrap();
 
     std::thread::scope(|s| {
-        s.spawn(|| {
-            loop {
-                let mut buf = [0; 1];
-                let count = stdin.read(&mut buf).unwrap();
-                let data = &buf[..count];
+        s.spawn(|| loop {
+            let mut buf = [0; 1];
+            let count = stdin.read(&mut buf).unwrap();
+            let data = &buf[..count];
 
-                eprintln!("Data: {:?}", data);
-                let _ = sp.lock().unwrap().write(data).unwrap();
-                sp.lock().unwrap().flush().unwrap();
-            }
+            eprintln!("Data: {:?}", data);
+            let _ = sp.lock().unwrap().write(data).unwrap();
+            sp.lock().unwrap().flush().unwrap();
         });
 
         loop {
             let mut buf = [0; 1];
             let count = match sp.lock().unwrap().read(&mut buf) {
-                    Ok(count) => count,
-                    Err(err) => match err.kind() {
-                        io::ErrorKind::TimedOut => continue,
-                        _ => panic!("{:?}", err),
-                    },
-                };
+                Ok(count) => count,
+                Err(err) => match err.kind() {
+                    io::ErrorKind::TimedOut => continue,
+                    _ => panic!("{:?}", err),
+                },
+            };
 
             let data = &buf[..count];
 
